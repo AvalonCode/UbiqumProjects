5d73c98647e9340dba2b424b70905ba627ec06b9
AvalonCode <alexgironasalmeron@gmail.com>
2019-03-26 16:51:39 +0100

    Create GameInfo.js

---
+++ Mobile Web App/GameInfo.js
@@ -0,0 +1,112 @@
+var data= 
+
+[
+  {
+    "SEPTEMBER": "9/01",
+    "Teams": "U1 and U4",
+    "Location": "AJ Katzenmaier",
+    "Times": "9:30 a.m."
+  },
+  {
+    "SEPTEMBER": "9/01",
+    "Teams": "U3 and U2",
+    "Location": "Greenbay",
+    "Times": "1:00 p.m."
+  },
+  {
+    "SEPTEMBER": "9/08",
+    "Teams": "U5 and U6",
+    "Location": "Howard A Yeager",
+    "Times": "9:30 a.m."
+  },
+  {
+    "SEPTEMBER": "9/08",
+    "Teams": "U6 and U1",
+    "Location": "Marjorie P Hart",
+    "Times": "1:00 p.m."
+  },
+  {
+    "SEPTEMBER": "9/15",
+    "Teams": "U2 and U4",
+    "Location": "North",
+    "Times": "9:30 a.m."
+  },
+  {
+    "SEPTEMBER": "9/15",
+    "Teams": "U3 and U5",
+    "Location": "AJ Katzenmaier",
+    "Times": "1:00 p.m."
+  },
+  {
+    "SEPTEMBER": "9/22",
+    "Teams": "U1 and U3",
+    "Location": "South",
+    "Times": "9:30 a.m."
+  },
+  {
+    "SEPTEMBER": "9/22",
+    "Teams": "U2 and U6",
+    "Location": "Howard A Yeager",
+    "Times": "1:00 p.m."
+  },
+  {
+    "SEPTEMBER": "9/29",
+    "Teams": "U4 and U5",
+    "Location": "Greenbay",
+    "Times": "9:30 a.m."
+  },
+  {
+    "OCTOBER": "OCTOBER",
+    "Teams": "Teams",
+    "Location": "Location",
+    "Times": "Times"
+  },
+  {
+    "OCTOBER": "10/06",
+    "Teams": "U2 and U5",
+    "Location": "Marjorie P Hart",
+    "Times": "9:30 a.m."
+  },
+  {
+    "OCTOBER": "10/06",
+    "Teams": "U1 and U6",
+    "Location": "South",
+    "Times": "1:00 p.m."
+  },
+  {
+    "OCTOBER": "10/13",
+    "Teams": "U3 and U4",
+    "Location": "Howard A Yeager",
+    "Times": "9:30 a.m."
+  },
+  {
+    "OCTOBER": "10/13",
+    "Teams": "U5 and U1",
+    "Location": "Greenbay",
+    "Times": "1:00 p.m."
+  },
+  {
+    "OCTOBER": "10/20",
+    "Teams": "U6 and U3",
+    "Location": "North",
+    "Times": "9:30 a.m."
+  },
+  {
+    "OCTOBER": "10/20",
+    "Teams": "U2 and U4",
+    "Location": "Marjorie P Hart",
+    "Times": "1:00 p.m."
+  },
+  {
+    "OCTOBER": "10/27",
+    "Teams": "U3 and U1",
+    "Location": "AJ Katzenmaier",
+    "Times": "9:30 a.m."
+  },
+  {
+    "OCTOBER": "10/27",
+    "Teams": "U5 and U6",
+    "Location": "Howard A Yeager",
+    "Times": "1:00 p.m."
+  }
+]
