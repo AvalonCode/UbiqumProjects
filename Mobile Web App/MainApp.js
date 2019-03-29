534cef72bf5e8e33ae243d7bac0f48d007bd9378
AvalonCode <alexgironasalmeron@gmail.com>
2019-03-29 15:56:31 +0100

    Update MainApp.js

--- Mobile Web App/MainApp.js
+++ Mobile Web App/MainApp.js
@@ -1,2 +1,16 @@
 
-// https://api.myjson.com/bins/111z1i
+var app = new Vue({
+	el: '#app',
+	data: {
+        all: []
+	}
+});
+
+
+fetch('https://api.myjson.com/bins/111z1i')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    app.all = data;
+  });
