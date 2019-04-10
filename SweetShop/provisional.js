a1699172ec937614bc5243f9a81a6a42209905a3
AvalonCode <alexgironasalmeron@gmail.com>
2019-04-10 15:51:08 +0200

    Update provisional.js

--- SweetShop/provisional.js
+++ SweetShop/provisional.js
@@ -1,14 +1,4 @@
 
-function getCollapsed(){
-    $('#collapseMe').collapse({
-        toggle: false
-    }
-
-    $('#collapseMe2').collapse({
-        toggle: true
-    }
-
-    $('#collapseMe3').collapse({
-        toggle: true
-    }
-}
+AOS.init({
+    duration: 1200,
+  })
